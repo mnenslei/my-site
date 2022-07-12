219be48dc047b1faeac8d08208945abe8a602a6a
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-07-12 17:19:52 -0400

    edited home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,11 @@ import '../index.css';
 const Home = () => {
     return (
         <div id='home' className='home-container'>
-            <h1>Hello 👋, my name is Michael Enslein. <br/> I am a fullstack web developer.</h1>
-            <p>Recently, I graduated from the Bloom Institute of Technology (fka Lambda School) and I am an alumnus of Indiana University-Bloomington, where I graduated from the Kelley School of Business.</p>
-            <p>Throughout my tech journey I have learned that practice makes perfect and I enjoy every second of putting in my reps to become a better engineer.</p>
-            <p>I built this portfolio site so that visitors may get a sense for my growth and journey in the engineering space.</p>
-            <p>So please, feel free to have a look around and if you would like to collaborate with me or have any questions please connect with me using any of the available options below in the footer.</p>
+            <h1>Hello 👋, my name is Michael Enslein and I am a fullstack web developer.</h1>
+            <p>I graduated from the Bloom Institute of Technology (fka Lambda School) in May 2022 and from Indiana University-Bloomington, Kelley School of Business in 2018.</p>
+            <p>Throughout my tech journey, I have always tried to look for new opportunities to learn and challenge myself to become a better engineer.</p>
+            <p>I built this portfolio site so that visitors might get a better sense to see and interact with some of my work.</p>
+            <p>Feel free to have a look around and if you would like to collaborate with me or have any questions, please reach out using any of the available options below in the footer.</p>
         </div>
         
     )
