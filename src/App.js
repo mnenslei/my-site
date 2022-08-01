8b96216ea73a4ff71c5ba59d1717fd4a193b6059
Michael Enslein <michael_enslein@Michaels-MacBook-Pro.local>
2022-08-01 14:17:59 -0400

    changed wording in home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Routes, Route} from 'react-router-dom'
 
 import Banner from "./components/Banner";
 import Home from "./components/Home";
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 <h1>Hello 👋, my name is Michael Enslein and I am a fullstack web developer.</h1>
                 <p>I graduated from the Bloom Institute of Technology (fka Lambda School) in May 2022 and from Indiana University-Bloomington, Kelley School of Business in 2018.</p>
                 <p>Throughout my tech journey, I have always tried to look for new opportunities to learn and challenge myself to become a better engineer.</p>
-                <p>I built this portfolio site so that visitors might get a better sense to see and interact with some of my work.</p>
+                <p>I built this portfolio site so that visitors might get a better opportunity to see and interact with some of my work.</p>
                 <p>Feel free to have a look around and if you would like to collaborate with me or have any questions, please reach out using any of the available options below in the footer.</p>
             </div>
             <div className='h-img'>
